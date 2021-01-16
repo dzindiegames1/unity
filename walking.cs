7da3942f056bdbd33d5fe8a6d4dd88d414f5f3a4
dzindiegames1 <74785236+dzindiegames1@users.noreply.github.com>
2021-01-16 16:55:16 +0100

    Add files via upload
    walk and rotate

--- walking.cs
+++ walking.cs
@@ -5,97 +5,70 @@ using UnityEngine;
 public class walking : MonoBehaviour
 {
 
+
     public float inputX;
     public float inputY;
+
     public float velocity;
     public float gravity = 5f;
-    public float speed = 10f;
-    public float jump_force = 10f;
+    public float _speed = 50f;
+    public float jump_Force = 10f;
     public CharacterController body;
-    public float _rotationSpeed = 10f;
+    public float _rotationSpeed = 180;
+    public Camera mainCamera;
+
+
 
     void awake()
     {
-        body = GetComponent<CharacterController>();
+        body = GetComponent<CharacterController>(); //asign controller to contoller
+
     }
-    void mouvePlayer()
+
+
+
+    private Vector3 rotation;
+    public void Update()
     {
         inputX = Input.GetAxis("Horizontal");
         inputY = Input.GetAxis("Vertical");
-        Vector3 mouveCharacter = new Vector3(inputX * speed, velocity
-            , inputY * speed) ;
-        body.Move(mouveCharacter * Time.deltaTime);
-        mouveCharacter.y = velocity;
-        Gravity();
-        //Rotation();
-    }
-    void Gravity()
-    {
+
         if (body.isGrounded)
         {
-            velocity -= gravity * Time.deltaTime;
+            velocity = -gravity * Time.deltaTime;
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                velocity = jump_force;
+                velocity = jump_Force;
             }
+
         }
         else
         {
-            velocity = gravity * Time.deltaTime;
+            velocity = -gravity * Time.deltaTime;
+
         }
+        Vector3 mouveCharacter = new Vector3(inputX * _speed, velocity, inputY * _speed);
+        body.Move(mouveCharacter * Time.deltaTime);
+        mouveCharacter.y = velocity;
+
+
+        Vector3 movementDirection = new Vector3(Input.GetAxisRaw("Horizontal")
+            , 0, Input.GetAxisRaw("Vertical"));
+        if (movementDirection != Vector3.zero)
+        {
+
+            //this works to make the character moves towards the direction 
+            this.transform.rotation = Quaternion.Euler(0, mainCamera.transform.rotation.eulerAngles.y, 0);
+            //this works to rotate the character in the direction of it's movment
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(
+                movementDirection), 0.15f);
+        }
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
