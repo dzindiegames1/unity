8ca8072aed325cb1c1d4a9ec141da14b816aa6c8
dzindiegames1 <74785236+dzindiegames1@users.noreply.github.com>
2021-01-30 21:03:41 +0100

    Add files via upload

---
+++ attack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+
+{
+  public Animator animator;
+    public CharacterController monster;
+   
+
+    // Start is called before the first frame update
+    void awake()
+    {
+        animator = GetComponent<Animator>();
+        monster = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+
+   
+
+    void Update()
+
+    {
+
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("Isattacking", true);
+        }
+        else
+        {
+            animator.SetBool("Isattacking", false);
+        }
+      
+
+    }
+
+}
